268aed2773e8b86c0202e227914b913eb926c231
Mefodei <mefodei.link@gmail.com>
2025-07-18 03:25:19 +0700

    - update converter api

--- Converter/Runtime/ProtoEcsMonoConverter.cs
+++ Converter/Runtime/ProtoEcsMonoConverter.cs
@@ -66,7 +66,7 @@ namespace UniGame.LeoEcs.Converter.Runtime
 #if ODIN_INSPECTOR
         [BoxGroup("converter settings")]
         [ShowIf(nameof(IsCustomWorld))]
-        [ValueDropdown(nameof(GetWorlds))]
+        [ValueDropdown(valuesGetter:nameof(GetWorlds),IsUniqueList = true)]
 #endif
         public string worldId = string.Empty;
         
